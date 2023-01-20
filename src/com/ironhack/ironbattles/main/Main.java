623c4d439e69324e3bd15f6c07e65a5ea1c58838
Boris Michael Randebrock <boris@randebrock.com>
2023-01-20 16:56:50 +0100

    testing develop branch

--- src/com/ironhack/ironbattles/main/Main.java
+++ src/com/ironhack/ironbattles/main/Main.java
@@ -2,6 +2,7 @@ package com.ironhack.ironbattles.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("You killed my father");
+        System.out.println("You killed my father....");
+        System.out.println("....prepare to die 😇");
     }
 }
